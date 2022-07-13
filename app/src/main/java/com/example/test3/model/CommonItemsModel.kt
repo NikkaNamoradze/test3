e2c759212a812043c2c4b85fcd01b8378b9ca7d0
nika <nika.namoradze.1@btu.edu.ge>
2022-07-13 11:25:58 +0400

    extract recourses

--- app/src/main/java/com/example/test3/model/CommonItemsModel.kt
+++ app/src/main/java/com/example/test3/model/CommonItemsModel.kt
@@ -1,4 +1,5 @@
 package com.example.test3.model
 
-data class CommonItemsModel(val startIcon: Int, val name: String, val language:String?, val endIcon: Int?){
+data class CommonItemsModel(val startIcon: Int, val name: String,val language:String?, val endIcon: Int?){
+//    data class CommonItemsModel(val startIcon: Int, val name: String, val language:String?, val endIcon: Int?)
 }
